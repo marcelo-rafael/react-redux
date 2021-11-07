34b07a076abcc80cf1fb8d2758fc28e53838d676
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2021-11-06 22:09:18 -0300

    Obtendo quantidade do carrinho

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,6 +1,19 @@
-import { all, takeLatest } from 'redux-saga/effects'
+import { all, takeLatest, select } from 'redux-saga/effects'
+import { IState } from '../..'
 
-function checkProductStock() {
+import { addProductToCart } from './actions'
+
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0
+  })
+
+  console.log(currentQuantity)
+
   console.log('Adicionou ao carrinho')
 }
 
